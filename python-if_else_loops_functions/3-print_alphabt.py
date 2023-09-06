cea487aa452073bfb83a37b2f1c10b1135c1fb51
allisonabinger <allisonabinger@gmail.com>
2023-09-06 10:07:03 -0700

    fixed it up real nice!

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97,122):
-	if i != 101 or i != 113
-	print({:c}.format(i))
+for i in range(ord('a'), ord('z') + 1):
+    if i == ord('q') or i == ord('e'):
+        continue
+    print("{:c}".format(i), end='')
