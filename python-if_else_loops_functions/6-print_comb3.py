6adcb3926f8cec25a2664c748d68c799c7750ca7
allisonabinger <allisonabinger@gmail.com>
2023-09-06 12:24:45 -0700

    fixed 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-	for n in range(10):
-		print("{:d} {:d}".format(i, n))
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        if num1 != 8 or num2 != 9:
+            print("{:d}{:d}".format(num1, num2), end=", ")
+        else:
+            print("{:d}{:d}".format(num1, num2))
