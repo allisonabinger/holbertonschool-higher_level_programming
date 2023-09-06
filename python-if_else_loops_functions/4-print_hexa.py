00e937582effc2f1f1079d412c8215f174183c92
allisonabinger <allisonabinger@gmail.com>
2023-09-06 11:24:52 -0700

    fixed decs

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-start_num = 0
-end_num = 99
-for num in range(start_num, end_num):
-	print(f"{:d} = 0x{:x}".format(i, i))
+for num in range(99):
+	print("{:d} = 0x{:x}".format(num, num))
