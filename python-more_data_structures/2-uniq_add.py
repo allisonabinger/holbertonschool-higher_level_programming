9d1a58bd1707b67fab510d819587ec1e98ab786d
allisonabinger <allisonabinger@gmail.com>
2023-09-12 10:02:19 -0700

    fixed return value in 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     new_list = []
     sum = 0
     for i in my_list:
-        if i not in my_list:
+        if i not in new_list:
             sum += i
             new_list.append(i)
-    return i
+    return sum
