752880101276e2db7514ff69c10dc02f30e868a6
allisonabinger <allisonabinger@gmail.com>
2023-09-19 15:37:54 -0700

    spelling error in 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     num = 0
     for i in range(1, len(sys.argv)):
-        add += int(sys.argv[i])
+        num += int(sys.argv[i])
     print("{:d}".format(num))
