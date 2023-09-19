e2f6d23cf0c004a3d84cb96ea9158385b5ebf01e
allisonabinger <allisonabinger@gmail.com>
2023-09-19 08:58:52 -0700

    pycode style

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	n = 0
-	for i in range(x):
-		try:
-			print("{:d}".format(my_list[i]), end="")
-			n += 1
-		except IndexError:
-			break
-	print("")
-	return (n)
+    r = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            r += 1
+        except IndexError:
+            break
+    print("")
+    return (r)
