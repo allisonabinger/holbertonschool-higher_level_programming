14df5b012dd4b852a1d30d67d02e31e7e796db86
allisonabinger <allisonabinger@gmail.com>
2023-09-19 14:57:22 -0700

    added repr to 4

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -59,3 +59,9 @@ class Rectangle:
             if i != self.__height - 1:
                 r.append("\n")
         return ("".join(r))
+
+    def __repr__(self):
+        """string representatoin of the rectangle"""
+        r = "Rectangle(" + str(self.__width)
+        r += ", " + str(self.height) + ")"
+        return (r)
