b6ae04a2aeeab6826a602f2c13e919da4a7040b4
allisonabinger <allisonabinger@gmail.com>
2023-09-19 11:30:00 -0700

    added 3, same as two but with area

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines class Square with specifications"""
+
+
+class Square:
+
+    """instance of a square"""
+
+    def __init__(self, size=0):
+        """initializes a new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns area of a square (size^2)"""
+        return self.__size ** 2
