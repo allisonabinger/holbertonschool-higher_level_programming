b940b2c639c3de1aeb59cb71b59d69890e1bb4a7
allisonabinger <allisonabinger@gmail.com>
2023-09-19 11:25:41 -0700

    pycode for 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """defines a square by listed attributes"""
 
+
 class Square:
-	"""instance of a square"""
-	def __init__(self, size = 0):
-		"""initializes a new square"""
-		
-		if not isinstance(size, int):
-			raise TypeError("size must be an integer")
-		elif size < 0:
-			raise ValueError("size must be >= 0")
-		self.__size = size
+
+    """instance of a square"""
+
+    def __init__(self, size=0):
+        """initializes a new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
