5cc0cbf294a2bebcf04f84310f043da0764a1ac0
allisonabinger <allisonabinger@gmail.com>
2023-09-26 15:03:00 -0700

    added sublass in 1, tests in txt

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Subclass named MyList inherited from list"""
+
+
+class MyList(list):
+    """subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
