b2536fc026bb62411ce3fa133aa57b6f2c9dbd8c
allisonabinger <allisonabinger@gmail.com>
2023-09-26 16:10:27 -0700

    added 3, isinstance, pycode

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""class and inherited class-checking function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if an object is an instance or inherited instance of a class
+
+        Args:
+            obj: object to check
+            a_class: class to reference
+        Returns:
+            true if either, false if not"""
+    if isinstance(obj, a_class):
+        return True
+    return False
