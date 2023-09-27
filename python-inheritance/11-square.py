9cba946496945eb53d3bc9d453c7705a9c0ee195
allisonabinger <allisonabinger@gmail.com>
2023-09-26 18:10:28 -0700

    implemented 11, pls work

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines square, subclass of rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represents a square"""
+
+    def __init__(self, size):
+        """initializes new square
+
+            size: (int) size of new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """str rep of a square"""
+        return f"[Square] {self.__size}/{self.__size}"
