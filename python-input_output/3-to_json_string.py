947b4d96646a15ede1ad37d3483fa8658f379d0c
allisonabinger <allisonabinger@gmail.com>
2023-09-28 07:22:26 -0700

    added 3- json derulo

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines json string serialization function"""
+import json
+
+def to_json_string(my_obj):
+    """serializes to json using dumps"""
+
+    json_string = json.dumps(my_obj)
+    return json_string
