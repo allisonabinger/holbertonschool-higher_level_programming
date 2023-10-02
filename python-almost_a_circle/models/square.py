f99300bd9849f3405cdfa49b37527ac1fbcf425d
allisonabinger <allisonabinger@gmail.com>
2023-10-02 07:43:44 -0700

    update method is function, to dictionary is too

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,3 +32,34 @@ class Square(Rectangle):
         "setter: sets width and height to the same size"
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """updates each attribute of square
+
+            args:
+                args(ints): new attribute values:
+                    1st: id
+                    2nd: width
+                    3rd: size
+                    4th: x
+                    5th: y
+                **kwargs (dict): new key/value pairs
+        """
+        if len(args) >= 1:
+            setattr(self, 'id', args[0])
+        if len(args) >= 2:
+            setattr(self, 'size', args[1])
+        if len(args) >= 3:
+            setattr(self, 'x', args[2])
+        if len(args) >= 3:
+            setattr(self, 'y', args[3])
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def to_dictionary(self):
+        return(
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+            )
