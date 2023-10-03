df7819c2c63be3e7aff71d63b2038806ecb5d973
allisonabinger <allisonabinger@gmail.com>
2023-10-03 06:49:03 -0700

    indentation/spacing

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,7 +26,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns json string from python dictionary"""
-
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         if type(list_dictionaries) is list:
