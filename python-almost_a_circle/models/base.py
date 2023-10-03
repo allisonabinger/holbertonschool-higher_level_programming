ff88b1b3fa19163320974d7f138fdb0c8fd979ae
allisonabinger <allisonabinger@gmail.com>
2023-10-03 06:34:38 -0700

    silly little spelling error

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns json string from python dictionary"""
-        if list_dictionaries is None or len(list_dictions) is 0:
+        if list_dictionaries is None or len(list_dictionaries) is 0:
             return ('"[]"')
         else:
             new_string = json.dumps(list_dictionaries)
