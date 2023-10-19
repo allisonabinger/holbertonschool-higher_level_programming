ed27f695e789d705e314d38ad22a9e153dbf3ff0
allisonabinger <allisonabinger@gmail.com>
2023-10-19 13:03:23 -0700

    1 - removed quotes

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates a user
-CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES on * . * to 'user_0d_1'@'localhost';
+CREATE IF NOT EXISTS USER user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES on * . * to user_0d_1@localhost;
