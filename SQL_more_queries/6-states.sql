12b5906016dde547f1024020ceb4169fb1979617
allisonabinger <allisonabinger@gmail.com>
2023-10-23 10:04:21 -0700

    states now has spaces and more comments

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,6 @@
 -- creates database hbtn_0d_usa and table sates in the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use database
 USE hbtn_0d_usa;
+--create table
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
