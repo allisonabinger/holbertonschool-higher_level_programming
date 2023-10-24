3ebb8b9a92e92b8d9803570aa4c85748aba9f85c
allisonabinger <allisonabinger@gmail.com>
2023-10-24 12:19:06 -0700

    fixed missing cokon in 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" Lists all states from db hbtn_0e_0_usa """
 
 
 import MySQLdb
 import sys
 
 
-def list_state(username, password, db_name, host, port):
-    
-    """ lists all states"""
-    db = MemoryError
+def list_states(username, password, database_name, host, port):
+
+    """ Listing states in db """
+    db = MySQLdb.connect(
+        user=username,
+        passwd=password,
+        db=database_name,
+        host=host,
+        port=port
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    results = cursor.fetchall()
+
+    for state in results:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+    list_states(username, password, database_name, host, port)
