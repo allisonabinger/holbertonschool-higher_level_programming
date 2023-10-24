0573886d4ca8ce01802815f6c2cb74203cc4aa1f
allisonabinger <allisonabinger@gmail.com>
2023-10-24 12:18:01 -0700

    trying with functino again

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """  lists all states from the database hbtn_0e_0_usa """
+
+
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-    cur.close()
-    db.close()
+def list_state(username, password, db_name, host, port):
+    
+    """ lists all states"""
+    db = MemoryError
