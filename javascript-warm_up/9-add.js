39b19eb636b3d4e441cd99326b540e9b6505a98b
Allison <allisonabinger@gmail.com>
2023-11-29 13:04:33 -0600

    a - 11/29 01:04 PM - 9 is drafted, untested

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,19 @@
+#!/usr/bin/local/node
+/**
+ * prints the addition of two integers
+ * first and second args are first and second integers, respectively
+ * uses function prototype: function add(a,b)
+ */
+function add (a,b) {
+  return a + b
+}
+const args = process.argv.slice(2);
+const x = +args[0];
+const y = +args[1];
+
+if (Number.isFinite(x) && Number.isInteger(x) && Number.isFinite(y) && Number.isInteger(y)) {
+  const sum = add(x, y);
+  console.log(sum);
+} else {
+  console.log('NaN');
+}
