b23dd75da731f83db8994ecd0d9479c39feebc67
Allison <allisonabinger@gmail.com>
2023-11-29 12:16:12 -0600

    a - 11/29 12:16 PM - semistandard for 2 - indentation errors

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 const args = process.argv.slice(2);
-//sliced 2 because first two are node and name of file
+// sliced 2 because first two are node and name of file
 if (args.length === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (args.length === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
