3ffc8e3776655951277e76c54d555cc245df06b5
Allison <allisonabinger@gmail.com>
2023-11-29 13:18:41 -0600

    a - 11/29 01:18 PM - 11 is drafted

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+/**
+ * searches the second biggest integer in the list of arguments passed to function
+ * if no argument passed, or if # of arguments is 1, prints 0
+ */
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const sorted = args.sort((a, b) => b - a);
+  console.log(sorted[1]);
+}
