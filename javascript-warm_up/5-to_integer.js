311c6ecd7cbd8d27386130819c5a4f7e6dc1e693
Allison <allisonabinger@gmail.com>
2023-11-29 12:35:04 -0600

    a - 11/29 12:34 PM - 5 - tested, passed semistandard

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/node
+/**
+ * prints "My number: <first arg converted to integer",
+ * if the first argument can be converted.
+ * if it cannot, prints "Not a number"
+ */
+const args = process.argv.slice(2);
+const arg1 = args[0];
+const num = +arg1;
+if (Number.isFinite(num) && Number.isInteger(num)) {
+  console.log('My number: ' + num);
+} else {
+  console.log('Not a number');
+}
