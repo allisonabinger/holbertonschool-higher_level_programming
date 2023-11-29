ec8ee78581106595382a156ec133741bfa7cf74f
Allison <allisonabinger@gmail.com>
2023-11-29 12:58:37 -0600

    a - 11/29 12:58 PM - 8 is drafted

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/local/bin/node
+/**
+ * prints a square using the 'X' character
+ * the length and width of the square is the first argument
+ * prints "Missing size" if argument cannot be converted into an integer
+ */
+const args = process.argv.slice(2);
+const size = +args[0];
+let h = 0;
+if (Number.isFinite(size) && Number.isInteger(size)) {
+  while (h < size) {
+    let row = '';
+    for (let l = 0; l < size; l++) {
+      row += 'X';
+    }
+    console.log(row);
+    h++;
+  }
+} else {
+  console.log('Missing size');
+}
