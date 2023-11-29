5e946048d8f7096be1997da1b1a440aae5a2cf74
Allison <allisonabinger@gmail.com>
2023-11-29 13:10:16 -0600

    a - 11/29 01:10 PM - 10 draft

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/local/bin/node
+/**
+ * computes and prints a factorial
+ * first arg is an integer used
+ * factorial of NaN is 1
+ * recursive function format
+ */
+const num = process.argv[2];
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+if (parseInt(num)) {
+  console.log(factorial(num));
+} else console.log(1);
