276896b951ab0807d347ebd2acbf1e95801020cf
Allison <allisonabinger@gmail.com>
2023-11-29 12:28:56 -0600

    a - 11/29 12:28 PM - draft of 4 - concat

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/local/bin/node
+/**
+ * prints two arguments passed to it
+ * in the following format: "arg1" is "arg2"
+ */
+const args = process.argv;
+// no slicing necessary
+console.log(args[2], 'is', args[3]);
