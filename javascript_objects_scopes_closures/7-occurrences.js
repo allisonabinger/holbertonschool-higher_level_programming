beec62d8cdb920046d53e862f4738f10348353c7
Allison <allisonabinger@gmail.com>
2023-11-29 22:34:17 -0600

    a - 11/29 10:34 PM - untested 7 - pushing to try?

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nb0ccurences = function (list, searchElement) {
+  let n = 0;
+  for (const element of list) {
+    if (element === searchElement){
+      n++;
+    }
+  }
+  return n;
+}
