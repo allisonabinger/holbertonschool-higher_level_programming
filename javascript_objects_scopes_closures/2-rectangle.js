7969410e412ba090db3068bcfa03053f469b7297
Allison <allisonabinger@gmail.com>
2023-11-29 22:00:04 -0600

    a - 11/29 09:59 PM - 2 is drafted

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/local/bin/node
+/**
+ * contains Rectangle class
+ * initializes width att with w, height att with h
+ * if w or h is <= 0, creates an empty object
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return this;// empty object
+    }
+  }
+}
