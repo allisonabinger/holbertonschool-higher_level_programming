1b378c01d6daeb9da6ff2345d24c0b2c7b0618a8
Allison <allisonabinger@gmail.com>
2023-11-29 22:34:55 -0600

    a - 11/29 10:34 PM - untested - semi passed - pushing to try?

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -2,9 +2,9 @@
 exports.nb0ccurences = function (list, searchElement) {
   let n = 0;
   for (const element of list) {
-    if (element === searchElement){
+    if (element === searchElement) {
       n++;
     }
   }
   return n;
-}
+};
