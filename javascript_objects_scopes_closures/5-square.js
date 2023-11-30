878e66949160e7edc7f0cf121b38929fab7cf306
Allison <allisonabinger@gmail.com>
2023-11-29 22:24:27 -0600

    a - 11/29 10:24 PM - untested 5 - passes semi

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -5,11 +5,11 @@
 const Rectangle = require('./4-rectangle');
 // imports the rectangle parent class
 
-class Square {
+class Square extends Rectangle {
   constructor (size) {
     // initializes using the constructor in rectangle
     // size = width and height
-    super(size, size)
+    super(size, size);
   }
 }
 module.exports = Square;
