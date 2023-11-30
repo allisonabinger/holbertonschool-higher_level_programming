d9d8d8d224c0760aa87e9b9ee6bcfaa616445649
Allison <allisonabinger@gmail.com>
2023-11-29 21:52:50 -0600

    a - 11/29 09:52 PM - 1 is added/tested

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * contains a class Rectangle
+ * takes two arguments w and h
+ * initializes instance att width with the value of w
+ * initializes instance att height with the value of h
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
