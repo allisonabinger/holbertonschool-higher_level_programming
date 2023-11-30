d668a2da9b2337ecb9f8548e44f850279856e51e
Allison <allisonabinger@gmail.com>
2023-11-29 22:35:34 -0600

    a - 11/29 10:35 PM - typo in 7

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nb0ccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let n = 0;
   for (const element of list) {
     if (element === searchElement) {
