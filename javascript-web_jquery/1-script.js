60fb9c44b4dd2d74365d1ed1fc431f6d5e425b73
Allison <allisonabinger@gmail.com>
2023-12-05 14:10:31 -0600

    A - 12/05 02:10 PM - testing 1 - js

--- javascript-web_jquery/1-script.js
+++ javascript-web_jquery/1-script.js
@@ -1,4 +1,6 @@
+#!/usr/bin/node
 /**
- * Using jQuery to update the text color of the <header> element.
+ * Updates the text color of the <header> element using jQuery API.
  * Based on 1-main.html
  */
+$('header').css('color', '#FF0000');
