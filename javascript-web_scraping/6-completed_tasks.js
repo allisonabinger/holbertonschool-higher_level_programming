233e92dcbf4a9a4177f8404ea21e92c2623d5221
Allison <allisonabinger@gmail.com>
2023-12-05 12:40:19 -0600

    A - 12/05 12:40 PM - 6 is ready! shebang fixed, semistandard passed

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,14 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 /**
  * Script that computes the number of tasks completed from an API
  * API: https://jsonplaceholder.typicode.com/todos given as first argument
  * Prints by User ID, only prints completed tasks.
+ * Currently prints map but has last bracket on extra newline.
  */
 const request = require('request');
 const url = process.argv[2];
 
-const completedList = {} /* Makes a map! Stores # of completed task by user */
+const completedList = {}; /* Makes a map! Stores # of completed task by user */
 /* Key = id, Value = number of tasks completed */
 
 request(url, (error, response, body) => {
@@ -24,9 +25,9 @@ request(url, (error, response, body) => {
       /* Checks if the completed value is not null/false in the API */
       completedList[task.userId] = (
         completedList[task.userId] || 0) + 1;
-        /* assigns the key/value pairs to completedList */
-        /* if user was not in list, they will be added with a count of 1 */
-      } 
-    });
-    console.log(completedList);
+      /* assigns the key/value pairs to completedList */
+      /* if user was not in list, they will be added with a count of 1 */
+    }
   });
+  console.log(completedList);
+});
