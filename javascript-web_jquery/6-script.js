8c36e73f1e39a87f3728ed65bb66cf952bd45619
Allison <allisonabinger@gmail.com>
2023-12-05 14:51:21 -0600

    A - 12/05 02:50 PM - updates header text when update_header is clicked

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+/**
+ * Updates the text of the <header> element when DIV#update_header is clicked
+ * Uses jQuery API, based on 6-main.html
+ */
+$('#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
