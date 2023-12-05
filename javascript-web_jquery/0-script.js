1cd97060510d83a876bc2842394412879608b12a
Allison <allisonabinger@gmail.com>
2023-12-05 14:24:08 -0600

    A - 12/05 02:23 PM - task 0 + 1 have better comments

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,6 +1,6 @@
 /**
  * selects <header> element using document.querySelector
  * then changes the font color to #FF0000.
- * Based on 0-main.html
+ * Based on 0-main.html.
  */
 document.querySelector('header').style.color = '#FF0000';
