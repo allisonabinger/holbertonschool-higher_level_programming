00c0d069e4b53897860b677509d1d36f8c9a29de
Allison <allisonabinger@gmail.com>
2023-12-04 18:17:48 -0600

    A - 12/04 06:17 PM - removed local from shebang

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Script that displays the status code of a GET request
+ * The first arugment is the url to request
+ */
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  console.log('code: ', response.statusCode);
+});
