84d164e15a1482c7d2ccae9a21832ec4f8b5528a
Allison <allisonabinger@gmail.com>
2023-12-05 14:30:23 -0600

    A - 12/05 02:30 PM - added 2-html, task 2 complete - header turns red when 'red-header' is clicked.

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,1 +1,8 @@
-/** */
+/**
+ * Script that updates the text color of the <header> element to red (#FF0000)
+ * whenever a user clicks on the tag DIV#red_header
+ * Uses jQuery API
+ */
+$('#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
